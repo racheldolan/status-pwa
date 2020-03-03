d618f7450da0fd5d4172ca45f1b08fbfc308c383
Rachel Dolan <rachel@jdlt.co.uk>
2020-03-03 13:34:02 +0000

    add files

---
+++ src/Status.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Item, Label, Segment, Divider, Icon } from "semantic-ui-react";
+import tubeMap from "./static/tubeMap";
+
+const Status = ({ status: { id, name, lineStatuses } }) => {
+  return (
+    <Segment
+      color={tubeMap[id] && `#${tubeMap[id].color}`}
+      basic
+      textAlign="center"
+      key={id}
+    >
+      <Item.Group>
+        <Item>
+          <Item.Content>
+            {tubeMap[id] && (
+              <Label
+              circular
+              empty
+                style={{
+                  backgroundColor: `#${tubeMap[id].color}`,
+                  marginRight: '0.5rem'
+                }}
+              />
+            )}
+            <Item.Header content={name} />
+            {lineStatuses.map(({ id, statusSeverityDescription }) => (
+              <>
+                <Item.Description key={id}>
+                  {statusSeverityDescription}
+                </Item.Description>
+                {statusSeverityDescription !== "Good Service" && (
+                  <Item.Extra>
+                    <Icon name="alarm" color="red" />
+                  </Item.Extra>
+                )}
+              </>
+            ))}
+          </Item.Content>
+        </Item>
+        <Divider />
+      </Item.Group>
+    </Segment>
+  );
+};
+export default Status;
